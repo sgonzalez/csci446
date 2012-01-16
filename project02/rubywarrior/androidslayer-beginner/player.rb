52bdbddb33508a9638fd21866de36fcc48a4d515
Santiago Gonzalez <santiagogonzalez@me.com>
2012-01-16 08:59:15 -0700

    Finished Level 4 of Ruby warrior

--- project02/rubywarrior/androidslayer-beginner/player.rb
+++ project02/rubywarrior/androidslayer-beginner/player.rb
@@ -1,13 +1,18 @@
 class Player
+@lastHealth = nil
   def play_turn(warrior)
+  	@lastHealth = warrior.health unless @lastHealth
     if warrior.feel.empty?
-      if warrior.health < 15
-        warrior.rest!
+      if warrior.health < @lastHealth
+        warrior.walk!
+      elsif warrior.health < 15
+      	warrior.rest!
       else
         warrior.walk!
       end
     else
       warrior.attack!
     end
+    @lastHealth = warrior.health
   end
 end
