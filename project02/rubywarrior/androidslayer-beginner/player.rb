ff5e6c6abbb21dda9acfc0b306ea3836e7d766a5
Santiago Gonzalez <santiagogonzalez@me.com>
2012-01-16 09:29:02 -0700

    Finished Level 6 of Ruby warrior

--- project02/rubywarrior/androidslayer-beginner/player.rb
+++ project02/rubywarrior/androidslayer-beginner/player.rb
@@ -1,12 +1,23 @@
 class Player
 @lastHealth = nil
+@hasRescued = false
   def play_turn(warrior)
   	@lastHealth = warrior.health unless @lastHealth
-    if warrior.feel.empty?
+  	if !@hasRescued
+  		if warrior.feel(:backward).wall?
+  			@hasRescued = true
+  			warrior.rest!
+  		elsif warrior.feel(:backward).captive?
+  			warrior.rescue!(:backward)
+  		else
+  			warrior.walk!(:backward)
+  		end
+    elsif warrior.feel.empty?
       if warrior.health < @lastHealth
         warrior.walk!
-      elsif warrior.health < 15
-      	warrior.rest!
+      elsif warrior.health < 20
+      	#warrior.rest!
+      	@hasRescued = false
       else
         warrior.walk!
       end
