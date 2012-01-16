3c23400d0f0de1a3ab99f933933479d6c46dbe56
Santiago Gonzalez <santiagogonzalez@me.com>
2012-01-16 08:54:57 -0700

    Finished Level 3 of Ruby warrior

--- project02/rubywarrior/androidslayer-beginner/player.rb
+++ project02/rubywarrior/androidslayer-beginner/player.rb
@@ -1,10 +1,13 @@
 class Player
   def play_turn(warrior)
-    # add your code here
     if warrior.feel.empty?
-    	warrior.walk!
+      if warrior.health < 15
+        warrior.rest!
+      else
+        warrior.walk!
+      end
     else
-    	warrior.attack!
+      warrior.attack!
     end
   end
 end
