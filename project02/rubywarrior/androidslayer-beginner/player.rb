75d241426daa834cbf8ea17cfe1dc8842d1d3997
Santiago Gonzalez <santiagogonzalez@me.com>
2012-01-20 08:48:49 -0700

    Finished Level 7 of Ruby warrior

--- project02/rubywarrior/androidslayer-beginner/player.rb
+++ project02/rubywarrior/androidslayer-beginner/player.rb
@@ -1,31 +1,35 @@
+#Professor Bakos,
+# I decided to redo much of the code, it was too complex and convoluted
+# I hope it is ok to do other functions
+
 class Player
-@lastHealth = nil
-@hasRescued = false
+  def initialize
+    @health = 0
+    @nostop = false
+  end
+
   def play_turn(warrior)
-  	@lastHealth = warrior.health unless @lastHealth
-  	if !@hasRescued
-  		if warrior.feel(:backward).wall?
-  			@hasRescued = true
-  			warrior.rest!
-  		elsif warrior.feel(:backward).captive?
-  			warrior.rescue!(:backward)
-  		else
-  			warrior.walk!(:backward)
-  		end
-    elsif warrior.feel.empty?
-      if warrior.health < @lastHealth
-        warrior.walk!
-      elsif warrior.health < 20
-      	#warrior.rest!
-      	@hasRescued = false
-      else
-        warrior.walk!
-      end
+    @nostop = true if warrior.health >= 20
+    if warrior.health < 20 && warrior.health >= @health
+      warrior.rest!
+      @warrior = false
+    elsif warrior.health < @health && warrior.feel.empty? && !@warrior
+      warrior.walk! :backward
+    elsif warrior.feel.wall?
+      warrior.pivot!
+    else
+      walk(warrior)
+    end
+    @health = warrior.health
+  end
+
+  def walk(warrior)
+    if warrior.feel.empty?
+      warrior.walk!
     elsif warrior.feel.captive?
-    	warrior.rescue!
+      warrior.rescue!
     else
       warrior.attack!
     end
-    @lastHealth = warrior.health
   end
 end
