da9d6cf57adc84cea3aa0eeb4d46efd412cc21b8
Santiago Gonzalez <santiagogonzalez@me.com>
2012-01-25 16:10:53 -0700

    FINALY VALIDATION WORKS

--- project03/depot/app/models/product.rb
+++ project03/depot/app/models/product.rb
@@ -2,8 +2,8 @@ class Product < ActiveRecord::Base
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates :title, uniqueness: true
-	validates :image_rul, allow_blank: ture, format: {
-		with: %{\.(gif|jpg|jpeg|png)$}i,
+	validates :image_url, allow_blank: true, format: {
+		with: %r{\.(gif|jpg|jpeg|png)$}i,
 		message: 'must be a URL for a GIF, JPG, or PNG image.'
 	}
 end
