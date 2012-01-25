d053b91271b1af4a62bea5207bb90824252c63b4
Santiago Gonzalez <santiagogonzalez@me.com>
2012-01-25 14:54:10 -0700

    Added test data (works now)

--- project03/depot/db/seeds.rb
+++ project03/depot/db/seeds.rb
@@ -8,4 +8,9 @@
 
 Product.delete_all
 
-Product.create(title: 'Super Book', description: %{<p>This book is a book that teaches you how to read tests and remember to remove dummy data during production runs and blah blah lorem ipsub dolor sit amet. <p>}, image_url: 'ruby.jpg', price 49.95)
+# . . .
+Product.create(title: 'Super Book',
+description:%{<p>This book is a book that teaches you.<p>},
+image_url: 'ruby.jpg',
+price: 49.95)
+# . . .
