0da17092a609da249e9f3660664ed45020b25b7c
Santiago Gonzalez <santiagogonzalez@me.com>
2012-01-26 11:18:29 -0700

    More css styling (has weird bug)

--- project03/depot/db/seeds.rb
+++ project03/depot/db/seeds.rb
@@ -10,12 +10,12 @@ Product.delete_all
 
 # . . .
 Product.create(title: 'Super Book',
-description:%{<p>This book is a book that teaches you.<p>},
+description:%{<p>This book is a book that teaches you. But do not ask me why I chose apple's address book icon, it just looks really cool! Anyway this is a book that is expensive compared to a penny. ;)<p>},
 image_url: 'http://upload.wikimedia.org/wikipedia/en/1/13/Address_Book_Icon.png',
 price: 49.95)
 # . . .
 Product.create(title: 'Kleenex',
-description:%{<p>Makes blowing your nose very <i>easy</i><p>},
+description:%{<p>Makes blowing your nose <i>very</i> <b>easy</b>. I like to use fancy HTML text formating in this description. Kleenex are very usefull when you are sick, they start out nice and clean and then they turn gooey and disgusting; YUCK!<p>},
 image_url: 'http://moneysavingmom.com/wp-content/uploads/2010/03/211_kleenex.jpg',
 price: 10.00)
 # . . .
