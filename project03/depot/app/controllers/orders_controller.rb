fd6f0c0603105795bd4f550a77c3b9f42e827e2d
Santiago Gonzalez <santiagogonzalez@me.com>
2012-02-01 13:40:20 -0700

    Finished checkout, yay

--- project03/depot/app/controllers/orders_controller.rb
+++ project03/depot/app/controllers/orders_controller.rb
@@ -28,6 +28,8 @@ class OrdersController < ApplicationController
     if @cart.line_items.empty?
       redirect_to store_url, notice: "Your cart is empty"
       return
+    else
+      @order = Order.new
     end
 
     respond_to do |format|
@@ -45,12 +47,16 @@ class OrdersController < ApplicationController
   # POST /orders.json
   def create
     @order = Order.new(params[:order])
+    @order.add_line_items_from_cart(current_cart)
 
     respond_to do |format|
       if @order.save
-        format.html { redirect_to @order, notice: 'Order was successfully created.' }
+        Cart.destroy(session[:cart_id])
+        session[:cart_id] = nil
+        format.html { redirect_to store_url, notice: 'Thank you for your order.' }
         format.json { render json: @order, status: :created, location: @order }
       else
+        @cart = current_cart
         format.html { render action: "new" }
         format.json { render json: @order.errors, status: :unprocessable_entity }
       end
