bf6d090be897fafd438ae5174409bd59c2183e12
Santiago Gonzalez <santiagogonzalez@me.com>
2012-02-06 12:01:00 -0700

    Added icon set

--- project04/articles/config/routes.rb
+++ project04/articles/config/routes.rb
@@ -1,5 +1,5 @@
 Articles::Application.routes.draw do
-  get "home/index"
+  resources :articles
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +50,7 @@ Articles::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'articles#index'
 
   # See how all your routes lay out with "rake routes"
 
