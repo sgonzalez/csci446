4bb9500f1f40955f6850ba964856328fa7bd6869
Santiago Gonzalez <santiagogonzalez@me.com>
2012-02-09 15:25:53 -0700

    Made articles table have cool scrolling

--- project04/articles/app/controllers/articles_controller.rb
+++ project04/articles/app/controllers/articles_controller.rb
@@ -35,6 +35,7 @@ class ArticlesController < ApplicationController
   # GET /articles/1/edit
   def edit
     @article = Article.find(params[:id])
+    @article.increment!(:edits);
   end
 
   # POST /articles
--- project04/articles/app/models/article.rb
+++ project04/articles/app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ActiveRecord::Base
   validates :title, :author, :body, presence: true
+  validates :author, exclusion: %w(Pat)
 end
