bddbfdff67a6b8a758bc79faad6316a478a77690
Santiago Gonzalez <santiagogonzalez@me.com>
2012-02-21 13:19:17 -0700

    Basic photo attachment functionality

--- project05/app/models/author.rb
+++ project05/app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true
+  
+  has_attached_file :photo
 end
--- project05/db/schema.rb
+++ project05/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120221200131) do
+ActiveRecord::Schema.define(:version => 20120221201555) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -25,8 +25,12 @@ ActiveRecord::Schema.define(:version => 20120221200131) do
 
   create_table "authors", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
 end
