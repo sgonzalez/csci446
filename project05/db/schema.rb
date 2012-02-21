c13fdee0446ab40deda6463d096d2b1449f53395
Santiago Gonzalez <santiagogonzalez@me.com>
2012-02-21 13:03:20 -0700

    Created scaffold for author

--- project05/db/schema.rb
+++ project05/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120202234654) do
+ActiveRecord::Schema.define(:version => 20120221200131) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(:version => 20120202234654) do
     t.text     "body"
     t.date     "creation"
     t.integer  "edits"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "authors", :force => true do |t|
+    t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
