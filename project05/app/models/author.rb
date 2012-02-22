8e1dcadaf30bd5b5538384d32e0c7265d675108f
Santiago Gonzalez <santiagogonzalez@me.com>
2012-02-22 09:22:38 -0700

    Better 'Pat' validation

--- project05/app/models/author.rb
+++ project05/app/models/author.rb
@@ -1,6 +1,12 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true
-  validates :author, exclusion: %w(Pat)
+  validate :no_author_named_pat
   
   has_attached_file :photo
+  
+  private
+    
+    def no_author_named_pat
+      self.errors.add(:base, "Must not be named Pat because of evil CEO.") if self.name.downcase.include? 'pat'
+    end
 end
