bc263ae98198601ba3ddaa45aa13391b6d0efb9b
Santiago Gonzalez <santiagogonzalez@me.com>
2012-02-27 12:54:00 -0700

    Removed evil plugin

--- project05/app/models/article.rb
+++
@@ -1,6 +0,0 @@
-class Article < ActiveRecord::Base
-  validates :title, :author, :body, presence: true
-  validates :author, exclusion: %w(Pat)
-  
-  belongs_to :author
-end
--- project05/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Articles::Application.config.secret_token = '74eb42e25f3ea0def7fd809c72e02b0babd4295a1f7ffddc9005a28b4c5d7ea59ad034af66bba88f8fcf8012bba0a2add8b3d0d473a7e332da780ad49c8469ea'
