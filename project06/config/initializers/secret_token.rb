8a39e89cbbfabc2187e48827344b0086fb7b7ec6
Santiago Gonzalez <santiagogonzalez@me.com>
2012-03-23 12:15:23 -0600

    Created empty project 6

---
+++ project06/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gamez::Application.config.secret_token = 'b5a6d818a857d8769f9773f4d870b24d339e148875deea6cb5f66bf9a3a1360e6bb74860e7c8f15957ba23461f595bced7595f473ac01473fab9c873aee0e9c1'
