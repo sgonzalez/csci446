19ffdb92a8e855ce6aca1e843001e9aceb313a13
Santiago Gonzalez <santiagogonzalez@me.com>
2012-03-25 12:56:06 -0600

    Captcha is displayed on registration form

---
+++ project06/config/initializers/recaptcha.rb
@@ -0,0 +1,8 @@
+#ENV['RECAPTCHA_PUBLIC_KEY'] = '6LeSY88SAAAAAMBhz8NOwLb6PC_pr8Fd7BT3geBK'
+#ENV['RECAPTCHA_PRIVATE_KEY'] = '6LeSY88SAAAAAPbZ_124jNrTwvMUIUhIOZ3goRqI'
+
+Recaptcha.configure do |config|
+  config.public_key  = '6LeSY88SAAAAAMBhz8NOwLb6PC_pr8Fd7BT3geBK'
+  config.private_key = '6LeSY88SAAAAAPbZ_124jNrTwvMUIUhIOZ3goRqI'
+  config.proxy = 'http://myrpoxy.com.au:8080'
+end
