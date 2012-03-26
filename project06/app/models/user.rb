9e46d794617616358d5e399be595af67010ec985
Santiago Gonzalez <santiagogonzalez@me.com>
2012-03-26 12:29:45 -0600

    Added better validation for registration

--- project06/app/models/user.rb
+++ project06/app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   #belongs to role
   
   validates_presence_of :first, :last
+  validates_length_of :username, :minimum => 6
+  validates_length_of :password, :minimum => 6
   
   has_attached_file :photo
   
