13712e24948a50460dc426b286e3c8b1965105c4
Santiago Gonzalez <slgonzalez@me.com>
2012-04-04 09:07:04 -0600

    Removed useless actions

--- project06/app/controllers/users_controller.rb
+++ project06/app/controllers/users_controller.rb
@@ -39,25 +39,4 @@ class UsersController < ApplicationController
       end
     end
   end
-  
-  # GET /users/1/edit
-  def edit
-    @user = current_user
-  end
-
-  # PUT /users/1
-  # PUT /users/1.json
-  def update
-    @user = User.find(params[:id])
-
-    respond_to do |format|
-      if @user.update_attributes(params[:user]) && verify_recaptcha(:model => @user)
-        format.html { redirect_to :users, notice: 'Successfully updated profile.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 end
